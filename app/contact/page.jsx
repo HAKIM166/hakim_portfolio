d88c130e57e3a26254ed99578b23b338b93d8766
HAKIM166 <ahmed.hakem2@yahoo.com>
2025-11-21 17:09:01 +0200

    Update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,49 +51,60 @@ export default function Contact() {
             rounded-xl"
             >
               <h3 className="text-4xl text-accent">Let’s work together </h3>
-              <p className="text-white/60">Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Corrupti eum sunt ut explicabo similique.</p>
-                {/* input */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                  <Input type="firstname" placeholder="Firstname" />
-                  <Input type="lastname" placeholder="Lastname" />
-                  <Input type="email" placeholder="Email address" />
-                  <Input type="phone" placeholder="Phone number" />
-                </div>
-                {/* select */}
-                <Select>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Sale a service"/>
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Select a service</SelectLabel>
-                      <SelectItem value="est"> Web Development </SelectItem>
-                      <SelectItem value="cst"> UI/UX Design </SelectItem>
-                      <SelectItem value="mst"> Logo Design </SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                {/* textarea */}
-                <Textarea className="h-[200px]" placeholder="Type your messege here."/>
-                {/* btn */}
-                <Button size="md" className="max-w-40">Send message</Button>
+              <p className="text-white/60">
+                Have a project or front-end role in mind? Share the details
+                below and I’ll get back to you as soon as possible.
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Sale a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est"> Web Development </SelectItem>
+                    <SelectItem value="cst"> UI/UX Design </SelectItem>
+                    <SelectItem value="mst"> Logo Design </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* textarea */}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your messege here."
+              />
+              {/* btn */}
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
             </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
-              {info.map((item,index) => {
-                return <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c]
-                   text-accent rounded-md flex items-center justify-center">
-                    <div className="text-[28px]">{item.icon}</div>
-                  </div>
-                  <div className="flex-1">
-                    <p className="text-white/60">{item.title}</p>
-                    <h3 className="text-xl">{item.description}</h3>
-                  </div>
-                </li>
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div
+                      className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c]
+                   text-accent rounded-md flex items-center justify-center"
+                    >
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
               })}
             </ul>
           </div>
